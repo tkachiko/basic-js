f2df770a9ba39103e7589f5d283337fe3f5b2e46
tkachiko <tkachikos@gmail.com>
2021-02-27 20:42:44 +0300

    feat: implement transform array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw new Error('Invalid argument')
+
+    const result = [];
+    result.push('null')
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-next') {
+            result.push('null');
+            i++;
+        } else if (arr[i] === '--discard-prev') {
+            result.pop();
+        } else if (arr[i] === '--double-next') {
+            if (i !== arr.length - 1) result.push(arr[i + 1]);
+        } else if (arr[i] === '--double-prev') {
+            result.push(result[result.length - 1]);
+        } else result.push(arr[i]);
+    }
+    return result.filter(el => el !== 'null');
 };
